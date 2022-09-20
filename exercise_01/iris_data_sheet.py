7f2926053ba72bee7908c0ad2c6844d3a11552c3
Anushka Milinda <y5heCH@pm.me>
2022-09-20 07:26:36 +0300

    add partial answer for question 1

---
+++ exercise_01/iris_data_sheet.py
@@ -0,0 +1,21 @@
+
+# read data
+data = pd.read_csv(
+    'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
+# labeling columns
+data.columns = ['Sepal length', 'Sepal width',
+                'Petal length', 'Petal width', 'Class']
+
+# this will print the column name
+# print(data.columns)
+
+
+min_data = data["Sepal length"].min()
+max_data = data["Sepal length"].max()
+
+print("Minimum:", min_data, "\nMaximum:", max_data)
+
+
+# print all the columns
+""" for c in data.columns:
+    print(c) """
