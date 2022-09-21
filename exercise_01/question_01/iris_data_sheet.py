5d0618f67d0c0a62688622aad661b1c06a784e90
Anushka Milinda <y5heCH@pm.me>
2022-09-21 19:55:22 +0300

    Complete the question 1

--- exercise_01/question_01/iris_data_sheet.py
+++ exercise_01/question_01/iris_data_sheet.py
@@ -1,28 +1,32 @@
 
 import pandas as pd
-# from tabulate import tabulate
+from tabulate import tabulate
 
 # read data
-data = pd.read_csv(
-    'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
-# labeling columns
+data = pd.read_csv('./iris.data')
+
+# labeling columns 
 data.columns = ['Sepal length', 'Sepal width',
                 'Petal length', 'Petal width', 'Class']
 
-# this will print the first few rows
-# print(data.head())
+# empty dictionary 
+data_dict = {" ": [], "Min": [], "Max": []}
 
-# print(data)
-
-""" min_data = data["Sepal length"].min()
-max_data = data["Sepal length"].max() """
-
-# print("Minimum:", min_data, "\nMaximum:", max_data)
-
-
-# print all the columns
+# fill data to 'data_dict'
 for c in data.columns[:4]:
     min = data.loc[data[c].idxmin()]
     max = data.loc[data[c].idxmax()]
-    print(f'{c} \t {min[c] :<6} ({min["Class"].split("-")[1]})\t\
-        {max[c]} ({max["Class"].split("-")[1]})')
+    data_dict[' '].append(c)
+    data_dict['Min'].append(f'{min[c]} ({min["Class"].split("-")[1]})')
+    data_dict['Max'].append(f'{max[c]} ({max["Class"].split("-")[1]})')
+
+# make dictionary to DataFrame
+df = pd.DataFrame(data_dict)
+
+# print "Summary Statistics" column 
+print(f'+{"-"*51}+')
+print("|{:>35}{:>17}".format("Summary Statistics", "|"))
+
+# print rest of the table 
+print(tabulate(df, headers='keys', tablefmt='grid',
+               showindex=False, colalign=("left", "left", "left")))
